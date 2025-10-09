c3b52c1e60e1422b67ca9faf14847e3d4108fc65
hencydepani03 <vaghasiyajenil29@gmail.com>
2025-10-09 12:41:03 +0530

    Extension has Been Completed

--- full_stack/JS/Yadas/index.js
+++ full_stack/JS/Yadas/index.js
@@ -1,16 +1,51 @@
 let myLeads = [];
+let oldLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputbtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deletebtn = document.getElementById("delete-btn");
+const tabbtn = document.getElementById("tab-btn");
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+
+deletebtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
 
 inputbtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  ulEl.innerHTML += `
-    <li>
-      <a target='_blank' href='${inputEl.value}'>
-        ${inputEl.value}
-      </a>
-    </li>
-  `;
-  inputEl.value = ""
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
 });
+
+tabbtn.addEventListener("click", function () {
+  // The query options are now in a single object
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    // 'tabs' will be an array of tabs that match the query.
+    // In this case, it should be an array with just one tab object.
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+      <li>
+        <a target='_blank' href='${leads[i]}'>
+          ${leads[i]}
+        </a>
+      </li>
+    `;
+  }
+  ulEl.innerHTML = listItems;
+}
